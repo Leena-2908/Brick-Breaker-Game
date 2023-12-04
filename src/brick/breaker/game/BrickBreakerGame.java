d15170018393222953b56717b413e6140ebd44cd
Leena <142425934+Leena-2908@users.noreply.github.com>
2023-12-04 19:05:04 +0530

    Initial commit

---
+++ src/brick/breaker/game/BrickBreakerGame.java
@@ -0,0 +1,18 @@
+
+package brick.breaker.game;
+import javax.swing.JFrame;
+public class BrickBreakerGame {
+
+    public static void main(String[] args) {
+      JFrame f=new JFrame();
+      f.setTitle("Brick Breaker");
+      f.setSize(700,600);
+      f.setLocationRelativeTo(null);
+      f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      f.setVisible(true);
+      f.setResizable(false);
+      GamePlay gamePlay =new GamePlay();
+      f.add(gamePlay);
+      f.addKeyListener(gamePlay);
+    }
+}
